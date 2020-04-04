552af6aebc47447fe402195f1412d57ab9a3e055
Felipe Serna <feserna86@gmail.com>
2020-04-04 15:57:44 +0000

    shell.h

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+int _putchar(char c);
+#endif /* SHELL_H */
