f896cdaba53647d8c956a22fc1361a794333ff7d
Felipe Serna <feserna86@gmail.com>
2020-04-06 22:09:11 +0000

    shell.h

--- shell.h
+++ shell.h
@@ -9,6 +9,27 @@
 #include <unistd.h>
 #include <stdarg.h>
 
+/**
+ * struct builtins - structure for built-ins
+ * @arg: arguments
+ * @line: line
+ * @env: environment
+ * @builtin: builtin
+ * Description: structure of shell
+ */
+typedef struct builtins
+{
+	char *arg;
+	void (*builtin)(char **args, char *line, char **env);
+} builtins_t;
+
 int _putchar(char c);
 void shell(int ac, char **av, char **env);
+void exit_shell(char **args, char *line, char **env);
+void env_shell(char **args, char *line, char **env);
+char *_getline(void);
+char **split_line(char *line);
+int check_for_builtins(char **args, char *line, char **env);
+int launch_prog(char **args);
+int builtins_checker(char **args);
 #endif /* SHELL_H */
