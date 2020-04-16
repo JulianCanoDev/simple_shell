936f46177fb31ba0e0f9cb318c149c7b75c5f4a1
julian <juliancano.dev@gmail.com>
2020-04-16 13:02:45 -0500

    chaging_3

--- shell.h
+++ shell.h
@@ -42,5 +42,6 @@ char *save_path(char *tmp, char *path);
 char *read_dir(char *er, struct dirent *s, char *fi, int l, char *p, char *t);
 char *_getenv(char *env);
 char *_strstr(char *haystack, char *needle);
+
 int _strlen(char *s);
 #endif
